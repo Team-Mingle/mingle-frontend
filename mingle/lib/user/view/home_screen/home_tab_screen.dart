ba5605be0bad00a2f19561641caaae0b3cf6c992
박상철 <doya02171@gmail.com>
2023-09-13 12:04:36 +0900

    carousel modified

--- mingle/lib/user/view/home_screen/home_tab_screen.dart
+++ mingle/lib/user/view/home_screen/home_tab_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class HomeTabScreen extends StatelessWidget {
+class HomeTabScreen extends StatefulWidget {
   const HomeTabScreen({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<HomeTabScreen> createState() => _HomeTabScreenState();
+}
+
+class _HomeTabScreenState extends State<HomeTabScreen> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  List imageList = [
+    "https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg",
+    "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_1280.jpg",
+    "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830_1280.jpg",
+    "https://cdn.pixabay.com/photo/2015/06/19/20/13/sunset-815270_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/01/08/05/24/sunflower-1127174_1280.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +41,10 @@ class HomeTabScreen extends StatelessWidget {
                   height: 24,
                 ),
               ),
-              onPressed: () {
-                // 첫 번째 버튼 클릭 시 동작을 정의
-              },
+              onPressed: () {},
             ),
           ),
           actions: <Widget>[
-            // 나머지 두 개의 아이콘을 오른쪽에 배치
             IconButton(
               icon: Align(
                 alignment: Alignment.center,
@@ -41,9 +54,7 @@ class HomeTabScreen extends StatelessWidget {
                   height: 24,
                 ),
               ),
-              onPressed: () {
-                // 두 번째 버튼 동작을 정의
-              },
+              onPressed: () {},
             ),
             Padding(
               padding: const EdgeInsets.only(right: 8.0),
@@ -56,20 +67,27 @@ class HomeTabScreen extends StatelessWidget {
                     height: 28,
                   ),
                 ),
-                onPressed: () {
-                  // 세 번째 버튼 동작을 정의
-                },
+                onPressed: () {},
               ),
             ),
           ],
         ),
       ),
+      // 스크롤 뷰
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            const SizedBox(height: 16.0), // 여백 추가
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Stack(
+                children: [
+                  sliderWidget(),
+                  sliderIndicator(),
+                ],
+              ),
+            ),
 
-            // 예시: 아래에 100개의 아이템을 나열
             ListView.builder(
               shrinkWrap: true, // 리스트뷰 크기를 내용에 맞게 조정
               physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
@@ -84,6 +102,67 @@ class HomeTabScreen extends StatelessWidget {
             const SizedBox(height: 169), // 여백 추가
           ],
         ),
+      ),
+    );
+  }
+
+  // 위젯 분리 필요
+  Widget sliderWidget() {
+    return CarouselSlider(
+      carouselController: _controller,
+      items: imageList.map(
+        (imgLink) {
+          return Builder(
+            builder: (context) {
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Image(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    imgLink,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ).toList(),
+      options: CarouselOptions(
+        height: 160,
+        viewportFraction: 1.0,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 4),
+        onPageChanged: (index, reason) {
+          setState(() {
+            _current = index;
+          });
+        },
+      ),
+    );
+  }
+
+  // 위젯 분리 필요
+  Widget sliderIndicator() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: imageList.asMap().entries.map((entry) {
+          return GestureDetector(
+            onTap: () => _controller.animateToPage(entry.key),
+            child: Container(
+              width: 12,
+              height: 12,
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color:
+                    Colors.white.withOpacity(_current == entry.key ? 0.9 : 0.4),
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
