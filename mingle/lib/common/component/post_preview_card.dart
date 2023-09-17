42ed65ad06955c366e0e0a73e8253d53d2db70dd
Sangchul Park <doya02171@gmail.com>
2023-09-17 22:18:10 +0900

    padding changed

--- mingle/lib/common/component/post_preview_card.dart
+++ mingle/lib/common/component/post_preview_card.dart
@@ -37,12 +37,16 @@ class PostPreviewCard extends StatelessWidget {
   }
 
   Widget buildDivider() {
-    final verticalPadding = cardType == CardType.home ? 10.0 : 8.0;
+    final verticalMargin = cardType == CardType.home ? 10.0 : 8.0;
+    final horizontalPadding = cardType == CardType.home ? 12.0 : 8.0;
 
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: verticalPadding),
-      height: 0.1,
-      color: Colors.black,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: verticalMargin),
+        height: 1.0,
+        color: GRAYSCALE_GRAY_01,
+      ),
     );
   }
 
@@ -53,133 +57,147 @@ class PostPreviewCard extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-        child: ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: postList.length,
-          itemBuilder: (context, index) {
-            final post = postList[index];
-            return Column(
-              children: [
-                Row(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: postList.length,
+        itemBuilder: (context, index) {
+          final post = postList[index];
+          return Column(
+            children: [
+              // 첫번째 줄에만 padding
+              if (index == 0)
+                SizedBox(
+                  height: cardType == CardType.home ? 20.0 : 16.0,
+                ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
                   children: [
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          post['title'] ?? '',
-                          style: const TextStyle(
-                            fontFamily: "Pretendard Variable",
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: GRAYSCALE_BLACK_GRAY,
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              post['title'] ?? '',
+                              style: const TextStyle(
+                                fontFamily: "Pretendard Variable",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: GRAYSCALE_BLACK_GRAY,
+                              ),
+                              textAlign: TextAlign.left,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
-                          textAlign: TextAlign.left,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
                         ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 6.0,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        post['content'] ?? '',
+                        style: const TextStyle(
+                          fontFamily: "Pretendard Variable",
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: GRAYSCALE_GRAY_05,
+                        ),
+                        textAlign: TextAlign.left,
+                        maxLines: getMaxLines(),
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 6.0,
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    post['content'] ?? '',
-                    style: const TextStyle(
-                      fontFamily: "Pretendard Variable",
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      color: GRAYSCALE_GRAY_05,
-                    ),
-                    textAlign: TextAlign.left,
-                    maxLines: getMaxLines(),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                const SizedBox(
-                  height: 6.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          post['nickname'] ?? '',
-                          style: const TextStyle(
-                            fontFamily: "Pretendard Variable",
-                            fontSize: 11,
-                            fontWeight: FontWeight.w400,
-                            color: GRAYSCALE_GREY_ORANGE,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        const SizedBox(width: 4.0),
-                        SvgPicture.asset(
-                          'assets/img/common/ic_dot.svg',
-                          width: 2,
-                          height: 2,
-                        ),
-                        const SizedBox(width: 4.0),
-                        Text(
-                          post['timestamp'] ?? '',
-                          style: const TextStyle(
-                            fontFamily: "Pretendard Variable",
-                            fontSize: 11,
-                            fontWeight: FontWeight.w400,
-                            color: GRAYSCALE_GREY_ORANGE,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                      ],
+                    const SizedBox(
+                      height: 6.0,
                     ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        SvgPicture.asset(
-                          'assets/img/common/ic_like.svg',
-                          width: 16,
-                          height: 16,
+                        Row(
+                          children: [
+                            Text(
+                              post['nickname'] ?? '',
+                              style: const TextStyle(
+                                fontFamily: "Pretendard Variable",
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                                color: GRAYSCALE_GREY_ORANGE,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            const SizedBox(width: 4.0),
+                            SvgPicture.asset(
+                              'assets/img/common/ic_dot.svg',
+                              width: 2,
+                              height: 2,
+                            ),
+                            const SizedBox(width: 4.0),
+                            Text(
+                              post['timestamp'] ?? '',
+                              style: const TextStyle(
+                                fontFamily: "Pretendard Variable",
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                                color: GRAYSCALE_GREY_ORANGE,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
                         ),
-                        Text(
-                          post['likeCounts'] ?? '',
-                          style: const TextStyle(
-                            fontFamily: "Pretendard Variable",
-                            fontSize: 11,
-                            fontWeight: FontWeight.w400,
-                            color: GRAYSCALE_GREY_ORANGE,
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        SvgPicture.asset(
-                          'assets/img/common/ic_comment.svg',
-                          width: 16,
-                          height: 16,
-                        ),
-                        Text(
-                          post['commentCounts'] ?? '',
-                          style: const TextStyle(
-                            fontFamily: "Pretendard Variable",
-                            fontSize: 11,
-                            fontWeight: FontWeight.w400,
-                            color: GRAYSCALE_GREY_ORANGE,
-                          ),
-                          textAlign: TextAlign.left,
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/img/common/ic_like.svg',
+                              width: 16,
+                              height: 16,
+                            ),
+                            Text(
+                              post['likeCounts'] ?? '',
+                              style: const TextStyle(
+                                fontFamily: "Pretendard Variable",
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                                color: GRAYSCALE_GREY_ORANGE,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            SvgPicture.asset(
+                              'assets/img/common/ic_comment.svg',
+                              width: 16,
+                              height: 16,
+                            ),
+                            Text(
+                              post['commentCounts'] ?? '',
+                              style: const TextStyle(
+                                fontFamily: "Pretendard Variable",
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                                color: GRAYSCALE_GREY_ORANGE,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ],
                 ),
-                if (index != postList.length - 1) buildDivider(),
-              ],
-            );
-          },
-        ),
+              ),
+              if (index != postList.length - 1) buildDivider(),
+              if (index == postList.length - 1)
+                const SizedBox(
+                  height: 20.0,
+                ),
+            ],
+          );
+        },
       ),
     );
   }
