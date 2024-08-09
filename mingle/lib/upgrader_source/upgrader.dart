79d5b836aa9e50b6c42a304c498e5e313c866460
CHUNG YUNSEONG <e0550449@u.nus.edu>
2024-08-10 00:53:04 +0800

    Fix upgrader

--- mingle/lib/upgrader_source/upgrader.dart
+++ mingle/lib/upgrader_source/upgrader.dart
@@ -283,7 +283,9 @@ class Upgrader with WidgetsBindingObserver {
   }
 
   bool blocked() {
-    return belowMinAppVersion() || versionInfo?.isCriticalUpdate == true;
+    return belowMinAppVersion() ||
+        versionInfo?.isCriticalUpdate == true ||
+        isMajorUpdate();
   }
 
   bool shouldDisplayUpgrade() {
@@ -337,6 +339,20 @@ class Upgrader with WidgetsBindingObserver {
       }
     }
     return rv;
+  }
+
+  bool isMajorUpdate() {
+    int installedVersion =
+        int.parse(state.packageInfo!.version.split('.').first);
+    int appStoreVersion =
+        int.parse(versionInfo!.appStoreVersion!.toString().split('.').first);
+    int installeSubVersion =
+        int.parse(state.packageInfo!.version.split('.')[2]);
+    int appStoreSubVersion =
+        int.parse(versionInfo!.appStoreVersion!.toString().split('.')[2]);
+    return installedVersion < appStoreVersion ||
+        (installedVersion == appStoreVersion &&
+            installeSubVersion < appStoreSubVersion);
   }
 
   bool isTooSoon() {
